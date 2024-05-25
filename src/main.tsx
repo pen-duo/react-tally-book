04fb35d134d228e2cda14aa1deba2756bacde284
pengyiduo <2411363896@qq.com>
2024-05-25 18:09:01 +0800

    feat: 填写路由配置

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
 import App from './App'
+import { router } from './routes/router'
 
 const div = document.getElementById('root') as HTMLElement
 
 const root = ReactDOM.createRoot(div)
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
